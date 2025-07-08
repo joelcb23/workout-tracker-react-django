d7548455312fb6554fea8062a8ea9f25d16d9ca9
Joel Cruz <joel.cruz.b03@gmail.com>
2025-07-07 18:27:45 -0600

    corrections urls

--- client/src/api/auth.api.js
+++ client/src/api/auth.api.js
@@ -1,10 +1,11 @@
 import api from "./api";
 
-export const loginRequest = async (data) => await api.post(`/auth/login`, data);
+export const loginRequest = async (data) =>
+  await api.post(`api/auth/login`, data);
 
 export const registerRequest = async (data) =>
-  await api.post(`/auth/register`, data);
+  await api.post(`/api/auth/register`, data);
 
-export const logoutRequest = async () => await api.post(`/auth/logout`);
+export const logoutRequest = async () => await api.post(`/api/auth/logout`);
 
-export const profileRequest = async () => await api.get(`/auth/profile`);
+export const profileRequest = async () => await api.get(`/api/auth/profile`);
--- client/src/api/routine.api.js
+++ client/src/api/routine.api.js
@@ -1,51 +1,54 @@
 import api from "./api";
 
 // Routine request
-export const getRoutineActiveRequest = async()=>
-  await api.get(`/routines/get-routine-active`);
+export const getRoutineActiveRequest = async () =>
+  await api.get(`/api/routines/get-routine-active`);
 
 export const getRoutinesRequest = async () =>
-  await api.get(`/routines/get-routines`);
+  await api.get(`/api/routines/get-routines`);
 
-export const createRoutineRequest = async (data)=>
-  await api.post(`/routines/create-routine`, data);
+export const createRoutineRequest = async (data) =>
+  await api.post(`/api/routines/create-routine`, data);
 
 export const activateRoutineRequest = async (routine_id) =>
-  await api.put(`/routines/activate-routine/${routine_id}`);
+  await api.put(`/api/routines/activate-routine/${routine_id}`);
 
-export const deleteRoutineRequest = async(routineId)=>
-  await api.delete(`/routines/delete-routine/${routineId}`);
-
+export const deleteRoutineRequest = async (routineId) =>
+  await api.delete(`/api/routines/delete-routine/${routineId}`);
 
 // Exercise request
 export const getExercisesRequest = async (routine_id) =>
-  await api.get(`/routines/routine/${routine_id}/get-exercises`);
+  await api.get(`/api/routines/routine/${routine_id}/get-exercises`);
 
 export const getExerciseRequest = async (routine_id, exercise_id) =>
-  await api.get(`/routines/routine/${routine_id}/get-exercise/${exercise_id}`);
+  await api.get(
+    `/api/routines/routine/${routine_id}/get-exercise/${exercise_id}`
+  );
 
 export const addExerciseRequest = async (routineId, data) =>
-  await api.post(`/routines/routine/${routineId}/add-exercise`, data);
+  await api.post(`/api/routines/routine/${routineId}/add-exercise`, data);
 
-export const updateExerciseRequest =async(routineId, exerciseId, data)=>
-  await api.put(`/routines/routine/${routineId}/update-exercise/${exerciseId}`, data);
+export const updateExerciseRequest = async (routineId, exerciseId, data) =>
+  await api.put(
+    `/api/routines/routine/${routineId}/update-exercise/${exerciseId}`,
+    data
+  );
 
-export const deleteExerciseRequest =async(routineId, exerciseId)=>
-  await api.delete(`/routines/routine/${routineId}/delete-exercise/${exerciseId}`);
-
+export const deleteExerciseRequest = async (routineId, exerciseId) =>
+  await api.delete(
+    `/api/routines/routine/${routineId}/delete-exercise/${exerciseId}`
+  );
 
 // Sets Request
 export const getSetsRequest = async (routine_id, exercise_id) =>
   await api.get(
-    `/routines/routine/${routine_id}/exercise/${exercise_id}/get-sets`
+    `/api/routines/routine/${routine_id}/exercise/${exercise_id}/get-sets`
   );
 
 export const markSetRequest = async (routine_id, exercise_id, set_id) =>
   await api.put(
-    `/routines/routine/${routine_id}/exercise/${exercise_id}/set/${set_id}/mark-set-done`
+    `/api/routines/routine/${routine_id}/exercise/${exercise_id}/set/${set_id}/mark-set-done`
   );
 
 export const unmarkSetRequest = async (routine_id) =>
-  await api.put(
-    `/routines/routine/${routine_id}/unmark-set-done`
-  );
+  await api.put(`/api/routines/routine/${routine_id}/unmark-set-done`);
